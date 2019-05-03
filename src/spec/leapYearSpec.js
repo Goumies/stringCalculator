7b547dccee9a77645bb45b0ec3e612f8db2cc908
romy alula <goumiesone.dev@gmail.com>
2019-05-03 14:19:39 +0200

    Add not a leap year test case

--- src/spec/leapYearSpec.js
+++ src/spec/leapYearSpec.js
@@ -1,14 +1,25 @@
 const chai = require('chai');
-
-const assert = chai.assert;
 const should = chai.should();
+
+function isDivisible(year, divisible) {
+    return year % divisible === 0;
+}
+
+function isleapYear(year) {
+    let divisible = 4;
+    if (isDivisible(year, divisible)) return true;
+
+    return false;
+}
 
 describe('Tests for isLeapYear method', () => {
 
    describe('not a leap year', () => {
 
        it('should return false, given a year not divisible by 4', () => {
-           (1+1).should.equal(2);
+           isleapYear(2005).should.equal(false);
+           isleapYear(2001).should.equal(false);
+           isleapYear(1993).should.equal(false);
        });
 
    });
